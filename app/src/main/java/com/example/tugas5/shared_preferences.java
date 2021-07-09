7fb2802d69043e4d3a9457195a484e8b5e2af6b4
satrianjayy <satriakusuma200@gmail.com>
2021-07-09 17:35:38 +0700

    Tugas 5 MC

---
+++ app/src/main/java/com/example/tugas5/shared_preferences.java
@@ -0,0 +1,33 @@
+package com.example.tugas5;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+/**
+ * Class for Shared Preference
+ */
+public class shared_preferences {
+    Context context;
+    shared_preferences(Context context) {
+        this.context = context;
+    }
+    public void saveLoginDetails(String email, String password) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences
+                ("LoginDetails", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("Email", email);
+        editor.putString("Password", password);
+        editor.commit();
+    }
+    public String getEmail() {
+        SharedPreferences sharedPreferences = context.getSharedPreferences
+                ("LoginDetails", Context.MODE_PRIVATE);
+        return sharedPreferences.getString("Email", "");
+    }
+    public boolean isUserLogedOut() {
+        SharedPreferences sharedPreferences = context.getSharedPreferences
+                ("LoginDetails", Context.MODE_PRIVATE);
+        boolean isEmailEmpty = sharedPreferences.getString("Email", "").isEmpty();
+        boolean isPasswordEmpty = sharedPreferences.getString("Password", "").isEmpty();
+        return isEmailEmpty || isPasswordEmpty;
+    }
+}
